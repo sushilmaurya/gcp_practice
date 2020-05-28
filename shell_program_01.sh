6ebf8f691a00ddaac3a4c1fe64ea2e360166e63d
sushilmaurya <sushil.maurya2009@gmail.com>
2020-05-28 10:16:07 +0800

    Update shell_program_01.sh

--- shell_program_01.sh
+++ shell_program_01.sh
@@ -1,1 +1,7 @@
 echo "Hello, How are you. This must be logged." >> 'sample.log'
+echo "~~~~~~~~~~~~~ Before Uploading File ~~~~~~~~~~~~~~~~~~~~~~~~~~"
+gsutil ls -r gs://dev_project_03
+
+gsutil cp 'sample.log' gs://dev_project_03
+echo "~~~~~~~~~~~~~ After Uploading File ~~~~~~~~~~~~~~~~~~~~~~~~~~"
+gsutil ls -r gs://dev_project_03
